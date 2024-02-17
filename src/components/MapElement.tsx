90de7e7f4694f00a6a67337491df737911b18443
tastrick <56053600+tastrick@users.noreply.github.com>
2024-02-17 16:57:39 -0700

    added top menu

--- src/components/MapElement.tsx
+++ src/components/MapElement.tsx
@@ -2,18 +2,46 @@ import { MapContainer, TileLayer,Marker, Popup} from 'react-leaflet'
 import React from "react";
 import styles from "@/styles/MapElement.module.scss";
 import 'leaflet/dist/leaflet.css';
-
-interface MapComponentProps {
-  initialPosition: [number, number];
+import { hostname } from '@/shared/utils';
+import LocationResponse from '@/shared/LocationResponse';
+interface MapComponentProps
+{
+  serverSideProps: LocationResponse,
+  otherProps: {
+    initialPosition: [number, number]
+  }
 }
 
-class MapElement extends React.Component<MapComponentProps> {
-  public render = () => {
-    const { initialPosition } = this.props;
+export const getServerSideProps = async (context: any): Promise<{ props: MapComponentProps | {} }> => {
+  const { id } = context.params;
 
+  try {
+    const response = await fetch(`${hostname}/api/locations/`);
+    const locations = await response.json();
+
+    return {
+      props: { locations },
+    };
+  }
+  catch (error) {
+    return {
+      props: {}
+    };
+  }
+};
+
+
+class MapElement extends React.Component<MapComponentProps> {
+  constructor(props:any){
+    super(props);
+    
+  }
+  public render = () => {
+    const { serverSideProps, otherProps} = this.props;
+    
     return (
       <div className={styles.container}>
-        <MapContainer className={styles.map}  center={initialPosition} zoom={13}
+        <MapContainer className={styles.map}  center={otherProps.initialPosition} zoom={13}
             zoomControl={false} >
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -2,12 +2,57 @@ import React from "react";
 import dynamic from "next/dynamic";
 import 'leaflet/dist/leaflet.css';
 
+import Categories from "../shared/Categories";
+import styles from "@/styles/Map.module.scss";
+
 const MapElementDyn = dynamic(() => import("../components/MapElement"), { ssr: false })
 
-class Map extends React.Component {
+
+class Map extends React.Component{
+  private keys;
+  private values;
+  constructor(props:any){
+    super(props);
+    this.state = {
+      selectedIndex:0
+    }
+    this.keys = Object.keys(Categories);
+    this.values = Object.values(Categories);
+   //this.selectedIndex = 0;
+    //.sideIcons = [icon,icon,icon,icon,icon,icon,icon];
+  }
+
   public render = () => {
+    
     return (
       <main className="flex-wrapper">
+        <div className = {styles.topMenuContainer}>
+            {this.keys.map((icon,i) => {
+                if (i == this.state.selectedIndex){
+                    return(
+                    <div className = {styles.button_background_selected}>
+                        <div className = {styles.icon_container_menu}>
+                        <img src = {'/favicon.ico'}></img>
+                        </div>
+                        <div className = {styles.text_container_menu}>
+                    {Object.values(Categories)[i]}
+                    </div>
+                    </div>)
+                }else{
+                return(
+                <div className = {styles.button_background} onClick = {(e) =>{
+                     this.setState({selectedIndex:i})}}>
+                    <div className = {styles.icon_container_menu}>
+                    <img src = '/favicon.ico'></img>
+                    </div>
+                    <div className = {styles.text_container_menu}>
+                    {Object.values(Categories)[i]}
+                    </div>
+                    
+                </div>)}
+                
+            })}
+        </div>
         <MapElementDyn initialPosition={[51.049999, -114.066666]} />
       </main>
     );
