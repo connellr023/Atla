cdcadc58a9d7fd70b16e2db8c6f833f5dfc19132
Connell Reffo <58084009+Crisp32@users.noreply.github.com>
2024-02-17 22:31:34 -0700

    Updated metadata and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,8 +12,9 @@ class App extends React.Component<{ Component: React.ComponentType<any>, pagePro
         <Head>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta name="CalgaryHacks2024" content="Who knows..." />
-          <title>CalgaryHacks2024</title>
+          <meta name="Atla" content="Calgary Hacks 2024 Web App" />
+          <link rel="icon" href="/atla-logo-small.svg" />
+          <title>Atla</title>
         </Head>
         <Component {...pageProps} />
       </>
