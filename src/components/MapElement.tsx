e29923ee23a8d3f5352e5d8f3e22d4125004147c
Connell Reffo <58084009+Crisp32@users.noreply.github.com>
2024-02-17 17:27:10 -0700

    Added SSR

--- src/components/MapElement.tsx
+++ src/components/MapElement.tsx
@@ -1,47 +1,22 @@
-import { MapContainer, TileLayer,Marker, Popup} from 'react-leaflet'
+import { MapContainer, TileLayer } from 'react-leaflet'
 import React from "react";
 import styles from "@/styles/MapElement.module.scss";
-import 'leaflet/dist/leaflet.css';
-import { hostname } from '@/shared/utils';
 import LocationResponse from '@/shared/LocationResponse';
+import 'leaflet/dist/leaflet.css';
+
 interface MapComponentProps
 {
-  serverSideProps: LocationResponse,
-  otherProps: {
-    initialPosition: [number, number]
-  }
+  locations: LocationResponse,
+  initialPosition: [number, number]
 }
 
-export const getServerSideProps = async (context: any): Promise<{ props: MapComponentProps | {} }> => {
-  const { id } = context.params;
-
-  try {
-    const response = await fetch(`${hostname}/api/locations/`);
-    const locations = await response.json();
-
-    return {
-      props: { locations },
-    };
-  }
-  catch (error) {
-    return {
-      props: {}
-    };
-  }
-};
-
-
 class MapElement extends React.Component<MapComponentProps> {
-  constructor(props:any){
-    super(props);
-    
-  }
   public render = () => {
-    const { serverSideProps, otherProps} = this.props;
-    
+    const { locations, initialPosition } = this.props;
+
     return (
       <div className={styles.container}>
-        <MapContainer className={styles.map}  center={otherProps.initialPosition} zoom={13}
+        <MapContainer className={styles.map} center={initialPosition} zoom={13}
             zoomControl={false} >
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,3 +1,4 @@
+import { hostname } from '@/shared/utils';
 import React from "react";
 import dynamic from "next/dynamic";
 import 'leaflet/dist/leaflet.css';
@@ -7,53 +8,71 @@ import styles from "@/styles/Map.module.scss";
 
 const MapElementDyn = dynamic(() => import("../components/MapElement"), { ssr: false })
 
+export const getServerSideProps = async () => {
+  try {
+    const response = await fetch(`${hostname}/api/locations`);
+    const locations = await response.json();
 
-class Map extends React.Component{
+    return {
+      props: {
+        locations: locations
+      },
+    };
+  }
+  catch (error) {
+    return {
+      props: {}
+    };
+  }
+}
+
+class Map extends React.Component<any, any>
+{
   private keys;
-  private values;
-  constructor(props:any){
+
+  constructor(props: any) {
     super(props);
+
     this.state = {
       selectedIndex:0
     }
+
     this.keys = Object.keys(Categories);
-    this.values = Object.values(Categories);
-   //this.selectedIndex = 0;
-    //.sideIcons = [icon,icon,icon,icon,icon,icon,icon];
   }
 
   public render = () => {
-    
+    const { locations } = this.props;
+
     return (
       <main className="flex-wrapper">
         <div className = {styles.topMenuContainer}>
-            {this.keys.map((icon,i) => {
-                if (i == this.state.selectedIndex){
+            {this.keys.map((_icon, i) => {
+                if (i === this.state.selectedIndex){
                     return(
-                    <div className = {styles.button_background_selected}>
-                        <div className = {styles.icon_container_menu}>
+                    <div key={i} className = {styles.button_background_selected}>
+                      <div className = {styles.icon_container_menu}>
                         <img src = {'/favicon.ico'}></img>
-                        </div>
-                        <div className = {styles.text_container_menu}>
-                    {Object.values(Categories)[i]}
+                      </div>
+                      <div className = {styles.text_container_menu}>
+                        {Object.values(Categories)[i]}
+                      </div>
                     </div>
-                    </div>)
+                  )
                 }else{
-                return(
-                <div className = {styles.button_background} onClick = {(e) =>{
-                     this.setState({selectedIndex:i})}}>
-                    <div className = {styles.icon_container_menu}>
+                return (
+                <div key={i} className={styles.button_background} onClick = {(e) =>{
+                  this.setState({selectedIndex:i})}}>
+                  <div className = {styles.icon_container_menu}>
                     <img src = '/favicon.ico'></img>
-                    </div>
-                    <div className = {styles.text_container_menu}>
+                  </div>
+                  <div className = {styles.text_container_menu}>
                     {Object.values(Categories)[i]}
-                    </div>
-                    
-                </div>)}
-                
+                  </div> 
+                </div>
+              )}
             })}
         </div>
-        <MapElementDyn initialPosition={[51.049999, -114.066666]} />
+        <MapElementDyn initialPosition={[51.049999, -114.066666]} locations={locations} />
       </main>
     );
   }
