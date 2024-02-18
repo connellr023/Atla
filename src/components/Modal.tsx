a438da050e1b1791a03a405fda3a286b2f6ea09d
Connell Reffo <58084009+Crisp32@users.noreply.github.com>
2024-02-18 09:03:05 -0700

    Updated welcome

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,7 +14,7 @@ const Modal: React.FC<ModalProps> = ({ onExit, title, children }) => {
       <div className={styles.background}>
         <div className={styles.content}>
           <div className={styles.title}><b>{title}</b></div>
-          <img onClick={onExit} src="/exit.png" alt="exit" />
+          <img className={styles.exit} onClick={onExit} src="/exit.png" alt="exit" />
           {children}
         </div>
       </div>
--- src/components/WelcomeModal.tsx
+++ src/components/WelcomeModal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "./Modal";
+import Image from "next/image";
 import styles from "@/styles/WelcomeModal.module.scss";
 
 interface WelcomeModalProps
@@ -14,6 +15,15 @@ const WelcomeModal: React.FC<WelcomeModalProps> = ({ onExit, title, message }) =
     <Modal onExit={onExit} title={title}>
       <br />
       <div className={styles.message}>{message}</div>
+      <br />
+      <Image
+        className={styles.welcomeLogo}
+        alt="Atla logo"
+        src="/atla-logo-full.svg"
+        width={70}
+        height={50}
+        priority
+      />
     </Modal>
   );
 }
